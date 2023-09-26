d7084e04f06d863568a77e56a95bb3131b9f5202
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-09-26 22:12:53 +0200

    update

--- firebase/src/routes/auth/+page.svelte
+++ firebase/src/routes/auth/+page.svelte
@@ -1,32 +1,25 @@
 <script lang="ts">
-	import { getFirebaseClient } from '$lib/firebase_client';
-	import { signInWithPopup, GithubAuthProvider } from 'firebase/auth';
+    import { getFirebaseClient } from "$lib/firebase_client";
+    import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-	let form: HTMLFormElement;
+    const auth = getFirebaseClient();
 
-	const auth = getFirebaseClient();
-
-	async function login(): Promise<void> {
-		try {
-			const cred = await signInWithPopup(auth, new GithubAuthProvider());
-			const token = await cred.user.getIdToken();
-			await auth.signOut();
-			const input = document.createElement('input');
-			input.type = 'hidden';
-			input.name = 'token';
-			input.value = token;
-			form.appendChild(input);
-			form.submit();
-		} catch (err) {
-			console.error(err);
-		}
-	}
+    async function login(form: HTMLFormElement): Promise<void> {
+        try {
+            const cred = await signInWithPopup(auth, new GoogleAuthProvider());
+            const token = await cred.user.getIdToken();
+            await auth.signOut();
+            form.token.value = token;
+            form.submit();
+        } catch (err) {
+            console.error(err);
+        }
+    }
 </script>
 
-<form method="post" bind:this={form} />
-<button
-	class="border rounded p-2 mt-10 bg-gray-800 text-white hover:bg-gray-700"
-	on:click={login}
->
-	Login using Github
-</button>
+<form method="post" on:submit|preventDefault={(e) => login(e.currentTarget)}>
+    <input name="token" type="hidden" />
+    <button class="border rounded p-2 mt-10 bg-gray-800 text-white hover:bg-gray-700">
+        Login using Google
+    </button>
+</form>
